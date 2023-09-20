5036504d02d1536f85dbdce803ba54dd62779e32
Komer @ Algebra <komersmile@gmail.com>
2023-09-20 19:47:04 +0200

    rješenje s boljom funkcijom

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,33 @@ import User from './components/user';
 function App() {
 
   const initialUsers = [
-    { name: "Marko", age: 18 },
-    { name: "Ivica", age: 30 },
-    { name: "Ana", age: 24 },
-    { name: "Nikola", age: 61 },
-    { name: "Roko", age: 19 }
+    { id: 1, name: "Marko", age: 18 },
+    { id: 2, name: "Ivica", age: 30 },
+    { id: 3, name: "Ana", age: 24 },
+    { id: 4, name: "Nikola", age: 61 },
+    { id: 5, name: "Roko", age: 19 }
   ]
 
   const [users, setUsers] = useState(initialUsers);
 
-  const changeName = []
-  for (let i = 0; i < users.length; i++) {
-    changeName.push(
-      (function (event) {
-        const newUsers = [...users];
-        newUsers[i].name = event.target.value;
-        setUsers(newUsers);
-      })
-    );
+  // const changeName = []
+  // for (let i = 0; i < users.length; i++) {
+  //   changeName.push(
+  //     (function (event) {
+  //       const newUsers = [...users];
+  //       newUsers[i].name = event.target.value;
+  //       setUsers(newUsers);
+  //     })
+  //   );
+  // }
+
+  const changeName = (event, index) => {
+    const newUsers = [...users];
+    newUsers[index].name = event.target.value;
+    setUsers(newUsers);
   }
 
-  const userParagraphs = users.map((user,index) => <User key={"userParagraph" + index} name={user.name} age={user.age} changeName={changeName[index]} id={"par"+index} />);
+  const userParagraphs = users.map((user,index) => <User key={user.id} name={user.name} age={user.age} changeName={(event) => changeName(event,index)} id={"par"+index} />);
 
   return (
     <>
